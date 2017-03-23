497c62a14838b7f8175657b79facc7a59e63ddc1
Michael Gunner <michael@neptik.com>
2017-03-23 14:34:28 +0000

    basic setup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bullets-Sass
 
 This is the pure Sass Bullets framework used in the Bullets WP starter theme.
+
+npm install bullets-sass
--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,15 @@ var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     sass = require('gulp-sass'),
     cssnano = require('gulp-cssnano'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
     jshint = require('gulp-jshint');
 
 // Compile Sass, Autoprefix and minify
 gulp.task('styles', function() {
-    return gulp.src('./assets/scss/**/*.scss')
+    return gulp.src('./scss/style.scss')
         .pipe(plumber(function(error) {
             gutil.log(gutil.colors.red(error.message));
             this.emit('end');
@@ -20,11 +22,11 @@ gulp.task('styles', function() {
             browsers: ['last 5 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./assets/css/'))
+        .pipe(gulp.dest('./css/'))
         .pipe(rename({suffix: '.min'}))
         .pipe(cssnano())
         .pipe(sourcemaps.write('.')) // Creates sourcemaps for minified styles
-        .pipe(gulp.dest('./assets/css/'))
+        .pipe(gulp.dest('./css/'))
 });
 
 gulp.task('default', function() {
