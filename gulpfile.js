7be35b91f436be742f0bf922e5b4298c50ba9696
Michael Gunner <michael@neptik.com>
2017-03-23 14:29:46 +0000

    basic setup

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+// Grab our gulp packages
+var gulp  = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    jshint = require('gulp-jshint');
+
+// Compile Sass, Autoprefix and minify
+gulp.task('styles', function() {
+    return gulp.src('./assets/scss/**/*.scss')
+        .pipe(plumber(function(error) {
+            gutil.log(gutil.colors.red(error.message));
+            this.emit('end');
+        }))
+        .pipe(sourcemaps.init()) // Start Sourcemaps
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./assets/css/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('.')) // Creates sourcemaps for minified styles
+        .pipe(gulp.dest('./assets/css/'))
+});
+
+gulp.task('default', function() {
+  gulp.start('styles');
+});
